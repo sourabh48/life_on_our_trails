2991ee533cc7bc01bde404a73629d2aec9553efa
ritwik-9-sarkar <70847031+ritwik-9-sarkar@users.noreply.github.com>
2020-11-21 23:09:08 +0530

    push

--- life_on_our_trails/Lib/site-packages/pageviews/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import HitCount
-
-
-class HitCountAdmin(admin.ModelAdmin):
-	list_display = ('url', 'hits')
-
-
-admin.site.register(HitCount, HitCountAdmin)
