581fa5a507d27d03566f3f2e3d6f494e844b2370
Sourabh Sarkar <30088198+sourabh48@users.noreply.github.com>
2020-11-26 02:28:55 +0530

    link update

--- static/js/instafeed.js
+++ static/js/instafeed.js
@@ -1,6 +1,5 @@
 var feed = new Instafeed({
-
-    accessToken: 'IGQVJYVi1wTndObGNqaGJmaWp1UlptOHVCcGszUHBlRmMwWVhXSFA5MG11MG5sRjV1emNTd1ZAyaGFzczFzR25JUDlQOTFzV01XQklPS0xfZA0t4QVI3MGJZAelc2UTZAIYnA2bHNMSVRrYzZALZAktnNVdmYwZDZD,
+    accessToken: 'IGQVJYRFNUWFFUOXFWRThKaHRHRThyNUh2NnFKTjExNWxQMXQwUVpyeVdpNzNwV3doOFNnUDQ1MnBSSFV5b2xTUlllNmtKUmRHYU02MHliNE1zSzlLdEk4TVU2QXRMTHVILTU1dWpwc0FiOGczMHNENgZDZD',
     template: '<div class="single-instaalbum"><a href="{{link}}"><img src="{{image}}" alt=""/><div class="instaalbum-info"><p>{{caption}}</p></div></a></div>',                 
     target: 'instafeed',
     resolution: 'standard_resolution',
